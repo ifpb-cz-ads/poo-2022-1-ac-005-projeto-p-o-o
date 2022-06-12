ffae1e540827b6d50aa3b6cc3f579eee73890cb5
Jonathan César <jonathanjog237@gmail.com>
2022-06-12 19:16:48 -0300

    Adicionando questões de Chagas e Baruque

---
+++ CBBAtividadePratica01.java
@@ -0,0 +1,20 @@
+class TesteIgualdade {
+    void teste() {
+        String frase1;
+        String frase2;
+
+        frase1 = new String("Testando a igualdade");
+        frase2 = new String("Testando a igualdade");
+
+        System.out.println("Frase 1: " + frase1);
+        System.out.println("Frase 2: " + frase2);
+        System.out.println("Frase 1 e Frase 2 são iguais? R: " + (frase1 == frase2) + "\n\n");
+    }
+}
+
+public class CBBAtividadePratica01 {
+    public static void main(String[] args) {
+        TesteIgualdade teste = new TesteIgualdade();
+        teste.teste();
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Atividade Colaborativa 6.0.3
+
+### Respostas do Livro Chagas, Baruque & Baruque (2010):
+
+- CBBAtividadePratica01
+- CBBAtividadePratica02
+- CBBAtividadePratica03
+- CBBAtividadePratica04
