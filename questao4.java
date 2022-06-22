7c0d3a49e58e2881ccd0ef38dc8c7476b154663e
Débora Fernandes C <95572299+Deboraaluna@users.noreply.github.com>
2022-06-22 19:43:59 -0300

    BMExercio04.java

---
+++ questao4.java
@@ -0,0 +1,11 @@
+enum LightBulbType {
+    LED, FLUORESCENT, INCANDESCENT,
+}
+
+class LightBulb {
+    int power; 
+    int lifetime; 
+    String brand;
+    String color;
+    LightBulbType type;
+}
